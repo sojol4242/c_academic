e447c9728d896d2b30297e7d1c24483c7fd8fe39
Md Moniruzzaman Sojol <bsmrstueee90@gmail.com>
2021-07-02 21:35:36 +0600

    basic data types

--- data_types/data_type.c
+++ data_types/data_type.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int main()
 {
+
+    /*
+    Programming is all about play with various types of data. we have many types of data types for storing data.
+    Each variable in C has an associated data type. Each data type requires different amounts of memory and has some specific operations which can be performed over it.
+    */
+
     int a = 65;
     char b = 'S';
-
     double c = 65.746;
 
-
     // above int, double, char are data types
+    // We can use the sizeof() operator to check the size of a variable. See the following C program for the usage of the various data types:
 
     printf("Hello Data Types\n");
 
