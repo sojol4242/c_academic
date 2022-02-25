6c573a6037c34826748153293cf2d4ca949d2101
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-02-25 13:11:13 +0600

    topics added

--- problems/stringSort.c
+++ problems/stringSort.c
@@ -3,21 +3,39 @@
 #define MAX_ARR_SIZE 20;
 #define MAX_NAME_SIZE 100;
 
+void nameInput(char str[], int count);
+void sortString(char str[], int count);
+void output(char str[], int count);
+
 int main()
 {
     int i, j, count;
     char str[MAX_ARR_SIZE][MAX_NAME_SIZE], temp[MAX_NAME_SIZE];
-    puts("How many students name you want to get?\n ");
-    scanf("%d", &count);
 
-    puts("Enter students name one by one: \n");
-    for (i = 0; i <= count; i++)
+    nameInput(str, count);
+    sortString(str, count);
+    output(str, count);
+
+    return 0;
+}
+// input function
+void nameInput(char str[], int count)
+{
+    puts("How many name you want to get?");
+    scanf("%d", &count);
+    puts("Enter name one by one:");
+    for (int i = 0; i <= count; i++)
     {
         gets(str[i]);
     }
-    for (i = 0; i <= count; i++)
+}
+// sorter function
+void sortString(char str[], int count)
+{
+    char temp[MAX_NAME_SIZE];
+    for (int i = 0; i <= count; i++)
     {
-        for (j = i + 1; j <= count; j++)
+        for (int j = i + 1; j <= count; j++)
         {
             if (strcmp(str[i], str[j]) > 0)
             {
@@ -27,11 +45,13 @@ int main()
             }
         }
     }
-    printf("Order of Sorted Students:");
-    for (i = 0; i <= count; i++)
+}
+// output function
+void output(char str[], int count)
+{
+    printf("Order of Sorted Students: ");
+    for (int i = 0; i <= count; i++)
     {
         puts(str[i]);
     }
-
-    return 0;
 }
