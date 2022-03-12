6b8bc219c59a17db52148171bc4b03465345bfb2
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-03-13 02:06:31 +0600

    variable && keywords

--- variable_keyword/constants.c
+++ variable_keyword/constants.c
@@ -1,12 +1,12 @@
-#include <stdio.h>  // link section
-#define PI 3.14  
+#include <stdio.h> // link section
+#define PI 3.14
 int main()
- // main function. All programme runs here
+// main function. All programme runs here
 {
-    const float pi=(float)22/7;
-    printf("Value of PI is %f (from local var) \n", pi);
-    printf("Value of PI is %f (from define var) \n", PI);
-//  Compile Time Error: Cannot modify a const object
+    const float pi = (float)22 / 7;
+    printf("Value  PI is %f (from local var) \n", pi);
+    printf("Value PI is %f (from define var) \n", PI);
+    //  Compile Time Error: Cannot modify a const object
 
-    return 0;  
+    return 0;
 }
