7f1e6bea106f2573e23ebb43c272fec8ed55e928
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-03-13 02:09:42 +0600

    Hello Worldgit add hello.c

--- hello.c
+++ hello.c
@@ -1,36 +1,27 @@
 // standard input output function  > doc section;
 
-#include <stdio.h>  // link section
-
+#include <stdio.h> // link section
 
 // definition section
 
-
 // global declaration part
 
-hello.c> hello.s>hello.i>hello.exe
+// hello.c> hello.s>hello.i>hello.exe
 
 int main()
- // main function. All programme runs here
+// main function. All programme runs here
 {
     printf("Hello C Programme\nWelcome to Dept of EEE \n");
 
     int n;
     scanf("%d", &n);
- 
+
     printf("%d", n++);
 
-    return 0; // 0=successfully run 1= unsuccessfully run
+    return 0; // 0=successfully run, 1= unsuccessfully run
 }
 
-
-
 // sub programme section
-
-
-
-
-
 
 // Header files
 /*
