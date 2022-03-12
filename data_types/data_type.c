72dbf6e217f2908b92d052162bc9fb88e8662a30
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-03-13 02:07:59 +0600

    data_types

--- data_types/data_type.c
+++ data_types/data_type.c
@@ -2,19 +2,19 @@
 int main()
 {
 
- /*
-    Programming is all about play with various types of data. we have many types of data types for storing data.
-    Each variable in C has an associated data type. Each data type requires different amounts of memory and has some specific operations which can be performed over it.
-    
-   Type of Data type
-   Basic Data Type >	int, char, float, double;
+    /*
+       Programming is all about play with various types of data. we have many types of data types for storing data.
+       Each variable in C has an associated data type. Each data type requires different amounts of memory and has some specific operations which can be performed over it.
 
-   Derived Data Type >	array, pointer, structure, union;
+      Type of Data type
+      Basic Data Type >	int, char, float, double;
 
-   Enumeration Data Type>	enum;
+      Derived Data Type >	array, pointer, structure, union;
 
-   Void Data Type>	void;
-*/
+      Enumeration Data Type>	enum;
+
+      Void Data Type>	void;
+   */
 
     int a = 65;
     char b = 'S';
@@ -41,8 +41,6 @@ int main()
         " Value is %lf and Size is %lu bytes.\n",
         c, sizeof(double));
     // can use sizeof(c) above as well
-
-    printf("That is the base of Data Types in C\n");
 
     //  [ Learn More<a href="https://www.javatpoint.com/data-types-in-c"> Here⏩</a>]
 
