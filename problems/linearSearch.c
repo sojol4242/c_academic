5ef3b4ac620bdf257a0278e04f2cd3e33b1b8889
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-03-19 01:45:42 +0600

    problems

--- problems/linearSearch.c
+++ problems/linearSearch.c
@@ -1,7 +1,9 @@
 // linear search
 #include <stdio.h>
 #define MAX_SIZE 100
-int linearSearch(int a[], int searchItem);
+int linearSearch(int a[], int searchItem, int size);
+int peakElement();
+void minmax(int arr[], int n);
 int main()
 {
 
@@ -15,18 +17,21 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
-    printf("Enter search item \n");
-    scanf("%d", &searchItem);
+    // printf("Enter search item \n");
+    // scanf("%d", &searchItem);
 
-    linearSearch(arr, searchItem);
+    // linearSearch(arr, searchItem, size);
+    int peak = peakElement(arr, size);
+    printf("Max value in %d position\n", peak);
+    minmax(arr, size);
 
     return 0;
 }
 
-int linearSearch(int a[], int searchItem)
+int linearSearch(int a[], int searchItem, int size)
 {
     int i;
-    for (i = 0; i < 6; i++)
+    for (i = 0; i < size; i++)
     {
         if (a[i] == searchItem)
         {
@@ -37,4 +42,46 @@ int linearSearch(int a[], int searchItem)
 
     printf("%d not found.", searchItem);
     return 0;
+}
+
+int peakElement(int arr[], int n)
+{
+    int max = arr[0];
+    int max_index;
+    for (int i = 1; i < n; i++)
+    {
+
+        if (max < arr[i])
+        {
+            max = arr[i];
+            max_index = i;
+        }
+    }
+    return max_index;
+}
+
+void minmax(int arr[], int n)
+{
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    for (int i = 1; i < n; i++)
+    {
+
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+
+    printf("min = %d, max=  %d", min, max);
 }
