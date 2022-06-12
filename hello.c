6318fd1e5424faa1e79cdc39bcc2505f51211ebe
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-06-12 10:57:54 +0600

    Hello world

--- hello.c
+++ hello.c
@@ -13,10 +13,11 @@ int main()
 {
     printf("Hello C Programme\nWelcome to Dept of EEE \n");
 
-    int n;
-    scanf("%d", &n);
+    // int n;
+    // scanf("%d", &n);
 
-    printf("%d", n++);
+    // printf("%d", n++);
+ 
 
     return 0; // 0=successfully run, 1= unsuccessfully run
 }
