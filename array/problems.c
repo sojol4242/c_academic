439ccb872188720d918656fb92adad704b756204
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-07-29 00:13:11 +0600

    Arrays in c

--- array/problems.c
+++ array/problems.c
@@ -1,34 +1,73 @@
 #include <stdio.h>
-void countRepeatedDigit(int N);
+void value(int arr[], int size);
+void value_dis(int arr[], int size);
+// #include <stdio.h>
+// void countRepeatedDigit(int N);
+// int main()
+// {
+
+//     int N;
+
+//     printf("Enter the number:");
+//     scanf("%d", &N);
+//     countRepeatedDigit(N);
+//     return 0;
+// }
+
+// // count repeatedDigit;
+// void countRepeatedDigit(int N)
+// {
+//     int seen[10] = {0};
+//     int rem;
+//     while (N > 0)
+//     {
+//         rem = N % 10;
+//         seen[rem] += 1;
+//         N = N / 10;
+//     }
+
+//     for (int i = 0; i < 10; i++)
+//     { // checking the number seen counts
+//         if (seen[i] == 0)
+//         {
+//             continue;
+//         }
+//         printf("%d seen %d times\n", i, seen[i]); // just returned the given numbers informations
+//     }
+// }
+
 int main()
 {
+    int size; // array size
+    int arr[size];
+    printf("Enter the value of arr size ");
+    scanf("%d",&size); // input array size
+   
 
-    int N;
+    // value(arr, size);
 
-    printf("Enter the number:");
-    scanf("%d", &N);
-    countRepeatedDigit(N);
+    // value_dis(arr, size);
+
     return 0;
 }
 
-// count repeatedDigit;
-void countRepeatedDigit(int N)
+void value(int arr[], int size)
 {
-    int seen[10] = {0};
-    int rem;
-    while (N > 0)
-    {
-        rem = N % 10;
-        seen[rem] += 1;
-        N = N / 10;
-    }
 
-    for (int i = 0; i < 10; i++)
-    { // checking the number seen counts
-        if (seen[i] == 0)
-        {
-            continue;
-        }
-        printf("%d seen %d times\n", i, seen[i]); // just returned the given numbers informations
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter array value\n");
+        scanf("%d", &arr[i]); // input array value
     }
+}
+
+void value_dis(int arr[], int size)
+{
+
+    for (int i = 0; i < size; i++) // output array value
+    {
+        printf("Value of array is \n");
+        printf("%d \n", arr[i]);
+    }
+ 
 }
